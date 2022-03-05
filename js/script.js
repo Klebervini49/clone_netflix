ff822a67ca7bb102f65832cff39e8f67681767af
Kleberson <klebervini49@gmail.com>
2022-03-05 06:57:23 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-function page1() {
+async function page1() {
     let page = 1,
         cod = '386e8e900b2b3cc4583a5b5d7317d4eb',
         url = `http://api.themoviedb.org/3/discover/movie?api_key=${cod}&sort_by=popularity.desc&include_adult=false&language=pt-BR&page=${page}`
-    const result = url => fetch(url)
-        .then(data => data.json())
-        .then(json => {
-            if (json['results']) {
-                console.log(json)
-                json.results.forEach(i => {
-                    let imagem = i.backdrop_path,
-                        poster = i.poster_path,
-                        titulo = i.title,
-                        nota = i.vote_average,
-                        dataLancamento = i.release_date,
-                        descricacao = i.overview,
-                        html = `<div class="swiper-slide">
+    let result = await fetch(url);
+    let json = await result.json();
+    if (json['results']) {
+        console.log(json)
+        json.results.forEach(i => {
+            let imagem = i.backdrop_path,
+                poster = i.poster_path,
+                titulo = i.title,
+                nota = i.vote_average,
+                dataLancamento = i.release_date,
+                descricacao = i.overview,
+                html = `<div class="swiper-slide">
                                     <img src='https://image.tmdb.org/t/p/w500${imagem}'>
                                     <div class='modalSlide' style='display: none'>
                                         <div class='poster'>
@@ -26,49 +25,48 @@ function page1() {
                                         <div class='nota'>Média de votos: ${nota}</div>
                                     </div>
                                 </div>`
-                    document.querySelector('.page1').innerHTML += html
-                });
-            }
-        })
+            document.querySelector('.page1').innerHTML += html
+        });
+    }
     result(url)
 }
 page1()
 
-function page2() {
-    let page = 2,
-        cod = '386e8e900b2b3cc4583a5b5d7317d4eb',
-        url = `http://api.themoviedb.org/3/discover/movie?api_key=${cod}&sort_by=popularity.desc&include_adult=false&language=pt-BR&page=${page}`
-    const result = (url) => fetch(url)
-        .then((data) => data.json())
-        .then((json) => {
-            if (json['results']) {
-                console.log(json)
-                json.results.forEach(i => {
-                    let imagem = i.backdrop_path,
-                        poster = i.poster_path,
-                        titulo = i.title,
-                        nota = i.vote_average,
-                        dataLancamento = i.release_date,
-                        descricacao = i.overview,
-                        html = `<div class="swiper-slide">
-                                    <img src='https://image.tmdb.org/t/p/w500${imagem}'>
-                                    <div class='modalSlide' style='display: none'>
-                                        <div class='poster'>
-                                            <img src='https://image.tmdb.org/t/p/w500${poster}'>
-                                        </div>
-                                        <div class='title'>Titulo: ${titulo}</div>
-                                        <div class='descri'>Descrição: ${descricacao}</div>
-                                        <div class='date'>Data de Lançamento: ${dataLancamento}</div>
-                                        <div class='nota'>Média de votos: ${nota}</div>
-                                    </div>
-                                </div>`
-                    document.querySelector('.page2').innerHTML += html
-                });
-            }
-        })
-    result(url)
-}
-page2()
+// function page2() {
+//     let page = 2,
+//         cod = '386e8e900b2b3cc4583a5b5d7317d4eb',
+//         url = `http://api.themoviedb.org/3/discover/movie?api_key=${cod}&sort_by=popularity.desc&include_adult=false&language=pt-BR&page=${page}`
+//     const result = (url) => fetch(url)
+//         .then((data) => data.json())
+//         .then((json) => {
+//             if (json['results']) {
+//                 console.log(json)
+//                 json.results.forEach(i => {
+//                     let imagem = i.backdrop_path,
+//                         poster = i.poster_path,
+//                         titulo = i.title,
+//                         nota = i.vote_average,
+//                         dataLancamento = i.release_date,
+//                         descricacao = i.overview,
+//                         html = `<div class="swiper-slide">
+//                                     <img src='https://image.tmdb.org/t/p/w500${imagem}'>
+//                                     <div class='modalSlide' style='display: none'>
+//                                         <div class='poster'>
+//                                             <img src='https://image.tmdb.org/t/p/w500${poster}'>
+//                                         </div>
+//                                         <div class='title'>Titulo: ${titulo}</div>
+//                                         <div class='descri'>Descrição: ${descricacao}</div>
+//                                         <div class='date'>Data de Lançamento: ${dataLancamento}</div>
+//                                         <div class='nota'>Média de votos: ${nota}</div>
+//                                     </div>
+//                                 </div>`
+//                     document.querySelector('.page2').innerHTML += html
+//                 });
+//             }
+//         })
+//     result(url)
+// }
+// page2()
 
 
 

e05626de3ea90e029b172fd2f134fe64188aa6cf
Kleberson <klebervini49@gmail.com>
2022-03-08 14:49:59 -0300

    add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function page1() {
         pageString = '.page1',
         title_page = 'Populares',
         API_KEY = '386e8e900b2b3cc4583a5b5d7317d4eb',
-        url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=true&language=pt-BR&page=${page}`;
+        url = `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=true&language=pt-BR&page=${page}`;
 
     document.querySelector('.title_page1 h1').innerText = title_page
 
@@ -39,6 +39,49 @@ function page1() {
     result(url)
 }
 page1()
+
+function page2() {
+    let page = 2,
+        title_page = 'teste',
+        pageTitle = 'page2',
+        pageString = '.page2',
+        API_KEY = '386e8e900b2b3cc4583a5b5d7317d4eb',
+        url = `http://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=true&language=pt-BR&page=${page}`;
+
+    document.querySelector(`.title_${pageTitle} h1`).innerText = title_page
+
+    const result = (url) => fetch(url)
+        .then((data) => data.json())
+        .then((json) => {
+            if (json['results']) {
+                console.log(json)
+                json.results.forEach(i => {
+                    let imagem = i.backdrop_path,
+                        poster = i.poster_path,
+                        titulo = i.title,
+                        descricacao = i.overview,
+                        dataLancamento = i.release_date,
+                        nota = i.vote_average,
+                        html = `<div class="swiper-slide">
+                                    <img src='https://image.tmdb.org/t/p/w500${imagem}'>
+                                    <div class='modalSlide' style='display: none'>
+                                        <div class='poster'>
+                                            <img src='https://image.tmdb.org/t/p/w500${poster}'>
+                                        </div>
+                                        <div class='title'>Titulo: ${titulo}</div>
+                                        <div class='descri'>Descrição: ${descricacao}</div>
+                                        <div class='date'>Data de Lançamento: ${dataLancamento}</div>
+                                        <div class='nota'>Média de nota: ${nota}</div>
+                                    </div>
+                                </div>`
+                    document.querySelector(pageString).innerHTML += html
+                });
+            }
+        })
+    result(url)
+}
+page2()
+
 
 // modal
 setTimeout(() => {

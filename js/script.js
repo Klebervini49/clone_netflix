ce6327db3d8fbf2c0d9ac7529835e34385da6548
Kleberson <klebervini49@gmail.com>
2022-03-08 14:19:07 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
-async function page1() {
+function page1() {
     let page = 1,
-        cod = '386e8e900b2b3cc4583a5b5d7317d4eb',
-        url = `http://api.themoviedb.org/3/discover/movie?api_key=${cod}&sort_by=popularity.desc&include_adult=false&language=pt-BR&page=${page}`
-    let result = await fetch(url);
-    let json = await result.json();
-    if (json['results']) {
-        console.log(json)
-        json.results.forEach(i => {
-            let imagem = i.backdrop_path,
-                poster = i.poster_path,
-                titulo = i.title,
-                nota = i.vote_average,
-                dataLancamento = i.release_date,
-                descricacao = i.overview,
-                html = `<div class="swiper-slide">
+        pageString = '.page1',
+        title_page = 'Populares',
+        API_KEY = '386e8e900b2b3cc4583a5b5d7317d4eb',
+        url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=true&language=pt-BR&page=${page}`;
+
+    document.querySelector('.title_page1 h1').innerText = title_page
+
+    const result = (url) => fetch(url)
+        .then((data) => data.json())
+        .then((json) => {
+            console.log(json)
+
+            if (json['results']) {
+                json.results.forEach(i => {
+                    let imagem = i.backdrop_path,
+                        poster = i.poster_path,
+                        titulo = i.title,
+                        descricacao = i.overview,
+                        dataLancamento = i.release_date,
+                        nota = i.vote_average,
+                        html = `<div class="swiper-slide">
                                     <img src='https://image.tmdb.org/t/p/w500${imagem}'>
                                     <div class='modalSlide' style='display: none'>
                                         <div class='poster'>
@@ -25,55 +32,13 @@ async function page1() {
                                         <div class='nota'>Média de votos: ${nota}</div>
                                     </div>
                                 </div>`
-            document.querySelector('.page1').innerHTML += html
-        });
-    }
+                    document.querySelector(pageString).innerHTML += html
+                });
+            }
+        })
     result(url)
 }
 page1()
-
-// function page2() {
-//     let page = 2,
-//         cod = '386e8e900b2b3cc4583a5b5d7317d4eb',
-//         url = `http://api.themoviedb.org/3/discover/movie?api_key=${cod}&sort_by=popularity.desc&include_adult=false&language=pt-BR&page=${page}`
-//     const result = (url) => fetch(url)
-//         .then((data) => data.json())
-//         .then((json) => {
-//             if (json['results']) {
-//                 console.log(json)
-//                 json.results.forEach(i => {
-//                     let imagem = i.backdrop_path,
-//                         poster = i.poster_path,
-//                         titulo = i.title,
-//                         nota = i.vote_average,
-//                         dataLancamento = i.release_date,
-//                         descricacao = i.overview,
-//                         html = `<div class="swiper-slide">
-//                                     <img src='https://image.tmdb.org/t/p/w500${imagem}'>
-//                                     <div class='modalSlide' style='display: none'>
-//                                         <div class='poster'>
-//                                             <img src='https://image.tmdb.org/t/p/w500${poster}'>
-//                                         </div>
-//                                         <div class='title'>Titulo: ${titulo}</div>
-//                                         <div class='descri'>Descrição: ${descricacao}</div>
-//                                         <div class='date'>Data de Lançamento: ${dataLancamento}</div>
-//                                         <div class='nota'>Média de votos: ${nota}</div>
-//                                     </div>
-//                                 </div>`
-//                     document.querySelector('.page2').innerHTML += html
-//                 });
-//             }
-//         })
-//     result(url)
-// }
-// page2()
-
-
-
-
-
-
-
 
 // modal
 setTimeout(() => {
@@ -88,4 +53,4 @@ setTimeout(() => {
             valorTemporario.style.display = 'none'
         })
     })
-}, 100)
+}, 200)
